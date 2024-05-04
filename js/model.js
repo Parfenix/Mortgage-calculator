aee2adfcccaf708a01d5c777032d0621ba1d3371
Denis Parfenov <Parfenix@gmail.com>
2024-05-05 01:37:12 +0300

    worked on transferring data from input to the model level with additional checks

--- js/model.js
+++ js/model.js
@@ -26,6 +26,17 @@ function getResults() {
 function setData(newData) {
     console.log('New data', newData);
 
+    if (newData.onUpdate === 'inputCost') {
+
+        if (newData.cost < data.minPrice) {
+            newData.cost = data.minPrice
+        };
+
+        if (newData.cost > data.maxPrice) {
+            newData.cost = data.maxPrice
+        };
+    } 
+
     data = {
         ...data,
         ...newData,
--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,6 +1,8 @@
+import updateModel from "./../utils/updateModel.js";
+
 function init(getData ) {
     const input = document.querySelector('#input-cost');
-
+ 
     const settings = {
         numeral: true,
         numeralThousandsGroupStyle: 'thousand',
@@ -12,7 +14,6 @@ function init(getData ) {
 
     input.addEventListener('input', function() {
         const value = +cleveInput.getRawValue();
-        console.log(value);
 
         // Checking the minimum and maximum price 
         if (value < getData().minPrice || value > getData().maxPrice) {
@@ -22,10 +23,15 @@ function init(getData ) {
         if (value >= getData().minPrice &&  value <= getData().maxPrice) {
             input.closest('.param__details').classList.remove('param__details--error');
         }
-    })
+
+        updateModel(input, {
+            cost: value,
+            onUpdate: 'inputCost', 
+        })
+    }) 
 
     input.addEventListener('change', function() {
-        const value = +cleveInput.getRawValue();
+        let value = +cleveInput.getRawValue();
 
         if (value > getData().maxPrice) {
             input.closest('.param__details').classList.remove('param__details--error');
@@ -36,6 +42,13 @@ function init(getData ) {
             input.closest('.param__details').classList.remove('param__details--error');
             cleveInput.setRawValue(getData().minPrice);
         }
+
+        value = +cleveInput.getRawValue();
+
+        updateModel(input, {
+            cost: value,
+            onUpdate: 'inputCost', 
+        })
     })
 } 
 
