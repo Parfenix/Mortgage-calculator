3b67c6b5968e587f91751abc6a9e6323aa760665
Denis Parfenov <Parfenix@gmail.com>
2024-05-05 01:15:44 +0300

    worked on validating an input field in a price range

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -5,9 +5,38 @@ function init(getData ) {
         numeral: true,
         numeralThousandsGroupStyle: 'thousand',
         delimiter: ' ',
-    };
+    }; 
 
-    new Cleave(input, settings);
-}
+    const cleveInput = new Cleave(input, settings);
+    cleveInput.setRawValue(getData().cost);
+
+    input.addEventListener('input', function() {
+        const value = +cleveInput.getRawValue();
+        console.log(value);
+
+        // Checking the minimum and maximum price 
+        if (value < getData().minPrice || value > getData().maxPrice) {
+            input.closest('.param__details').classList.add('param__details--error');
+        }
+
+        if (value >= getData().minPrice &&  value <= getData().maxPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+        }
+    })
+
+    input.addEventListener('change', function() {
+        const value = +cleveInput.getRawValue();
+
+        if (value > getData().maxPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+            cleveInput.setRawValue(getData().maxPrice);
+        }
+
+        if (value < getData().minPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+            cleveInput.setRawValue(getData().minPrice);
+        }
+    })
+} 
 
 export default init;
